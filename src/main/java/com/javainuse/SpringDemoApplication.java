3c2f738fd68063e972e3f739d062db19ad80a0ec
Sunita2428 <suni24kumari@gmail.com>
2023-04-16 12:38:59 -0400

    first demo

--- src/main/java/com/javainuse/SpringDemoApplication.java
+++ src/main/java/com/javainuse/SpringDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.springdemo;
+package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class SpringDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class, args);
     }
-
 }
